d7470a0e408c172728eaf1fceba7522a417dd6d4
João Walter Amadeu <joaowa2002@live.com>
2021-03-10 15:06:11 -0300

    Primeiro Commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function initial(){
+    var bgc = document.getElementsByTagName("body")[0];
+    var btst = document.getElementById("mtbtn");
+    bgc.classList.add("font-monospace");
+    btst.style.textAlign = "left";
+
+}
+function settheme(){
+    var bgc = document.getElementsByTagName("body")[0];
+    var btst = document.getElementById("mtbtn");
+    var nav = document.getElementById("nav1");
+    if(bgc.style.backgroundColor=="black"){
+        bgc.style.backgroundColor = "white";
+        bgc.style.color = "black";
+        nav.classList.remove("navbar-dark");
+        nav.classList.remove("bg-dark");
+        nav.classList.add("navbar-light");
+        nav.classList.add("bg-light");
+        btst.classList.remove("btn-dark");
+        btst.classList.add("btn-light");
+        btst.innerHTML = "<i class='gg-sun'></i>Modo noturno";
+    }else{
+        bgc.style.color = "white";
+        bgc.style.backgroundColor = "black";
+        nav.classList.remove("navbar-light");
+        nav.classList.remove("bg-light");
+        nav.classList.add("navbar-dark");
+        nav.classList.add("bg-dark");
+        btst.classList.add("btn-dark");
+        btst.classList.remove("btn-light");
+        btst.innerHTML = "<i class='gg-moon'></i>Modo diurno";
+    }
+}
