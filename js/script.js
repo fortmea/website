19dd57ae9428f62fe5fd9131bc3907d16fd438ed
João Walter Amadeu <joaowa2002@live.com>
2021-03-12 14:26:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,14 @@
 var temac = localStorage.getItem('temac') | 0;
+if(window.location.pathname=="index.html"){
 $.ajax({
   type: 'POST',
-  url: 'https://xue-hua-piao.herokuapp.com/usuario/',
-  data: { 
-      'id': ''
-  },
+  url: 'https://xue-hua-piao.herokuapp.com/post/',
   dataType: 'json',
   success: function(msg){
       console.log("Mensagem:"+ (JSON.stringify(msg)));
   }
 });
+}
 jQuery(document).ready(function(){
     $( "#loader" ).delay(600).fadeOut(400, function(){
         $( "#corpo" ).delay(200).fadeIn(400);$("#corpo").css("visibility", "visible");
