e4b9a759249057f053b104616a492e29a9bac0a7
João Walter Amadeu <joaowa2002@live.com>
2021-03-12 14:27:47 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var temac = localStorage.getItem('temac') | 0;
-if(window.location.pathname=="index.html"){
+if(window.location.pathname=="/index.html"){
 $.ajax({
   type: 'POST',
   url: 'https://xue-hua-piao.herokuapp.com/post/',
