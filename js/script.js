5c4ac6f9b5cf5abde80b964a845ff8882087b60d
João Walter Amadeu <joaowa2002@live.com>
2021-03-13 19:12:41 -0300

    new theme changing algorithm

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 var temac = localStorage.getItem('temac') | 0;
-var getName = prompt("Diga-me, visitante... Qual seu nome?");
-if((window.location.pathname=="/index.html")||(window.location.pathname=="/")){
+//if((window.location.pathname=="/index.html")||(window.location.pathname=="/")){
 $.ajax({
   type: 'POST',
   url: 'https://xue-hua-piao.herokuapp.com/post/',
@@ -11,25 +10,28 @@ $.ajax({
         var target = document.getElementById( "post-container" );
         var newElement = document.createElement( "div" );
         newElement.style = "padding:1em;";
-        if(getName){
-            post[x].nome = post[x].nome.replace("Anônimo!",getName+"!");
-        }
+        //if(getName){
+        //    post[x].nome = post[x].nome.replace("Anônimo!",getName+"!");
+        //}
         min = Math.ceil(15000);
         max = Math.floor(1);
         var rand = Math.floor(Math.random() * (max - min + 1)) + min;
-        newElement.innerHTML = ('<div class="card bg-dark border-info text-light font-monospace" onload="nomeautor('+post[x].autor+');" style="padding=1em"><h5 class="card-header bg-dark-50">'+post[x].nome+'</h5><div class="card-body bg-dark"><h5 class="card-title">Ainda não sei o que colocar aqui...</h5><p class="card-text">'+post[x].conteudo+'</p><p><cite id="autor'+post[x].autor+" "+rand+'""></cite></p><a onclick=read('+post[x].id+') class="btn btn-primary">Go somewhere</a></div> </div>');
+        var data = new Date(post[x].data);
+        data.setSeconds(0, 0);
+        var stamp = data.toISOString().replace(/T/, " ").replace(/:00.000Z/, "");
+        newElement.innerHTML = ('<div class="card bg-dark border-info font-monospace" style="padding=1em"><h5 class="card-header bg-dark bg-gradient">'+post[x].nome+'</h5><div class="card-body bg-dark"><h5 class="card-title">'+post[x].resumo+'</h5><p class="card-text">'+post[x].conteudo+'</p><p><img></img><cite id="autor'+post[x].autor+" "+rand+'""><i class="gg-loadbar-alt"></i></cite><br>'+stamp+'</p><a onclick=read('+post[x].id+') class="btn btn-primary">Ir para publicação</a></div> </div>');
         nomeautor(post[x].autor,rand);
         $( target ).after( newElement );
     }
   }
 });
-}
+//}
 function nomeautor(id,rand){
   let data2 = autor(id);
   data2.then(function(data3){
     var autor = document.getElementById("autor"+id+" "+rand);
     var autordata = jQuery.parseJSON((JSON.stringify(data3)))['data'];
-    autor.innerHTML = autordata.nome;
+    autor.innerHTML ="- "+ autordata.nome;
   });
   return id;
 }
@@ -51,4 +53,70 @@ jQuery(document).ready(function(){
 function initial(){
     var bgc = document.getElementsByTagName("body")[0];
     bgc.classList.add("font-monospace");
+    if(Cookies.get('tema_claro')){
+      tema_claro();
+    }else if(Cookies.get('tema_escuro')){
+      Cookies.get('tema_escuro');
+    }
+}
+function tema_claro(){
+    var list = document.getElementsByClassName("bg-dark");
+    var listabtn = document.getElementsByClassName("btn-dark");
+    var listafrmctrl = document.getElementsByClassName("form-control");
+    var listacard = document.getElementsByClassName("card");
+    for(i = 0;i<listafrmctrl.length;i++){
+      listafrmctrl[i].classList.replace('bg-dark','bg-light');
+    }
+    for(i = 0;i<listacard.length;i++){
+      listacard[i].classList.replace('bg-dark','bg-light');
+    }
+    for(i = 0;i<listabtn.length;i++){
+      listabtn[i].classList.replace('btn-dark','btn-light');
+      if(listabtn[i].innerHTML=='<i class="gg-moon"></i>Modo diurno'){
+        listabtn[i].innerHTML = '<i class="gg-sun"></i>Modo noturno';
+      }
+    }
+    var i = 0;
+    while(i<list.length){
+      if(list[i].tagName == 'NAV'){
+        list[i].classList.replace('navbar-dark','navbar-light');
+      }
+        list[i].classList.replace('bg-dark','bg-light');
+      i++;
+    }
+    document.getElementsByTagName("body")[0].style.color = "black";
+    document.getElementsByTagName("body")[0].style.classList = "bg-light container text-dark";
+    Cookies.set("tema_claro", 'light', { expires: 7, path: '/',domain: '.joaowalteramadeu.me' });
+    Cookies.remove('tema_escuro', { path: '', domain: '.joaowalteramadeu.me' });
+}
+function tema_escuro(){
+    var list = document.getElementsByClassName("bg-light");
+    var listabtn = document.getElementsByClassName("btn-light");
+    var listafrmctrl = document.getElementsByClassName("form-control");
+    var listacard = document.getElementsByClassName("card");
+    for(i = 0;i<listafrmctrl.length;i++){
+      listafrmctrl[i].classList.replace('bg-light','bg-dark');
+    }
+    for(i = 0;i<listacard.length;i++){
+      listacard[i].classList.replace('bg-light','bg-dark');
+    }
+    for(i = 0;i<listabtn.length;i++){
+      listabtn[i].classList.replace('btn-light','btn-dark');
+      if(listabtn[i].innerHTML=="<i class='gg-sun'></i>Modo noturno"){
+        listabtn[i].innerHTML = "<i class='gg-moon'></i>Modo diurno";
+      }
+    }
+    var i = 0;
+    while(i<list.length){
+      if(list[i].tagName == 'NAV'){
+        list[i].classList.replace('navbar-light','navbar-dark');
+      }
+      list[i].classList.replace('bg-light','bg-dark');
+      i++;
+    }
+    document.getElementsByTagName("body")[0].style.color = "white";
+    document.getElementsByTagName("body")[0].style.classList = "bg-dark container text-light";
+    Cookies.set("tema", 'dark', { expires: 7, path: '/',domain: '.joaowalteramadeu.me' });
+    Cookies.remove('tema_claro', { path: '', domain: '.joaowalteramadeu.me' });
+    
 }
