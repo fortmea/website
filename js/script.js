ef4011fd51ec91a585131b01122b714826ae7fdf
João Walter Amadeu <joaowa2002@live.com>
2021-03-13 12:51:40 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var temac = localStorage.getItem('temac') | 0;
 var getName = prompt("Diga-me, visitante... Qual seu nome?");
-if((window.location.pathname=="/index.html")||(window.location.pathname=="/")){
+//if((window.location.pathname=="/index.html")||(window.location.pathname=="/")){
 $.ajax({
   type: 'POST',
   url: 'https://xue-hua-piao.herokuapp.com/post/',
@@ -15,14 +15,14 @@ $.ajax({
         if(getName){
             post[x].nome = post[x].nome.replace("Anônimo!",getName+"!");
         }
-        newElement.innerHTML = ('<div class="card bg-dark border-info text-light font-monospace" style="padding=1em"><h5 class="card-header">'+post[x].nome+'</h5><div class="card-body bg-dark"><h5 class="card-title">Ainda não sei o que colocar aqui...</h5><p class="card-text">'+post[x].conteudo+'</p><p><cite>João</cite></p><a onclick=read('+post[x].id+') class="btn btn-primary">Go somewhere</a></div> </div>');
+        newElement.innerHTML = ('<div class="card bg-dark border-info text-light font-monospace" style="padding=1em"><h5 class="card-header bg-dark-50">'+post[x].nome+'</h5><div class="card-body bg-dark"><h5 class="card-title">Ainda não sei o que colocar aqui...</h5><p class="card-text">'+post[x].conteudo+'</p><p><cite>João</cite></p><a onclick=read('+post[x].id+') class="btn btn-primary">Go somewhere</a></div> </div>');
         $( target ).after( newElement );
       //console.log(msg[i]);
       //console.log("Mensagem:"+ (JSON.stringify(msg)));
     }
   }
 });
-}
+//}
 jQuery(document).ready(function(){
     $( "#loader" ).delay(600).fadeOut(400, function(){
         $( "#corpo" ).delay(200).fadeIn(400);$("#corpo").css("visibility", "visible");
