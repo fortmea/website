2a7a452e42d2191074264a885b14e668cbeb2f79
João Walter Amadeu <joaowa2002@live.com>
2021-03-13 23:21:57 -0300

    Final Iteration of the theme toggling system

--- js/script.js
+++ js/script.js
@@ -21,8 +21,9 @@ $.ajax({
         var stamp = data.toISOString().replace(/T/, " ").replace(/:00.000Z/, "");
         newElement.innerHTML = ('<div class="card bg-dark border-info font-monospace" style="padding=1em"><h5 class="card-header bg-dark bg-gradient">'+post[x].nome+'</h5><div class="card-body bg-dark"><h5 class="card-title">'+post[x].resumo+'</h5><p class="card-text">'+post[x].conteudo+'</p><p><img></img><cite id="autor'+post[x].autor+" "+rand+'""><i class="gg-loadbar-alt"></i></cite><br>'+stamp+'</p><a onclick=read('+post[x].id+') class="btn btn-primary">Ir para publicação</a></div> </div>');
         nomeautor(post[x].autor,rand);
-        $( target ).after( newElement );
+        $(target).after(newElement);
     }
+    tema(true);
   }
 });
 //}
@@ -53,15 +54,15 @@ jQuery(document).ready(function(){
 function initial(){
   var bgc = document.getElementsByTagName("body")[0];
   bgc.classList.add("font-monospace");
-  tema(true);
+ 
 }
 function tema(ini){
 if(!ini){
   temac++;
   localStorage.setItem('temac',temac);
 }
-for(var a = 0;a<4;a++){
-  if(temac%2==0){
+for(var a = 0;a<5;a++){
+  if(temac%2==1){
     tema_escuro();
   }else{
     tema_claro();
@@ -69,68 +70,59 @@ for(var a = 0;a<4;a++){
 }
 }
 function tema_claro(){
-  var z = 0;
-  do{
     try{
-    var list = document.getElementsByClassName("bg-dark");
-    var listabtn = document.getElementsByClassName("btn-dark");
+    document.getElementById('corpo').classList = "bg-light container text-dark";
     var listafrmctrl = document.getElementsByClassName("form-control");
-    var listacard = document.getElementsByClassName("card");
     for(var a = 0; a< listafrmctrl.length;a++){
       listafrmctrl[a].classList.replace('bg-dark','bg-light');
     }
+    var listacard = document.getElementsByClassName("card");
     for(var b = 0; b< listacard.length;b++){
       listacard[b].classList.replace('bg-dark','bg-light');
     }
+    var listabtn = document.getElementsByClassName("btn-dark");
     for(var b = 0; b < listabtn.length;b++){
       listabtn[b].classList.replace('btn-dark','btn-light');
-      if(listabtn[b].innerHTML=='<i class="gg-moon"></i>Modo diurno'){
-        listabtn[b].innerHTML = '<i class="gg-sun"></i>Modo noturno';
-      }
     }
-    for(c =0; c<list.length;c++){
+    var botaotema = document.getElementById('temabtn');
+    botaotema.innerHTML = '<i class="gg-sun"></i>Modo noturno';
+    var list = document.getElementsByClassName("bg-dark");
+    for(c = 0; c < list.length;c++){
       if(list[c].tagName == 'NAV'){
         list[c].classList.replace('navbar-dark','navbar-light');
       }
         list[c].classList.replace('bg-dark','bg-light');
     }
-    document.getElementById('corpo').classList = "bg-light container text-dark";
-    z++;
   }catch(error){
     return;
   }
-}while(z<=150)
 }
 function tema_escuro(){
   var z = 0;
-  do{
     try{
-    var list = document.getElementsByClassName("bg-light");
-    var listabtn = document.getElementsByClassName("btn-light");
+    document.getElementById('corpo').classList = "bg-dark container text-light";
     var listafrmctrl = document.getElementsByClassName("form-control");
-    var listacard = document.getElementsByClassName("card");
     for(var i =0; i<listafrmctrl.length;i++){
       listafrmctrl[i].classList.replace('bg-light','bg-dark');
     }
+    var listacard = document.getElementsByClassName("card");
     for(var a = 0; a < listacard.length;a++){
       listacard[a].classList.replace('bg-light','bg-dark');
     }
+    var listabtn = document.getElementsByClassName("btn-light");
     for(var b =0; b < listabtn.length;b++){
       listabtn[i].classList.replace('btn-light','btn-dark');
-      if(listabtn[i].innerHTML=="<i class='gg-sun'></i>Modo noturno"){
-        listabtn[i].innerHTML = "<i class='gg-moon'></i>Modo diurno";
-      }
     }
+    var botaotema = document.getElementById("temabtn");
+    botaotema.innerHTML = "<i class='gg-moon'></i>Modo diurno";
+    var list = document.getElementsByClassName("bg-light");
     for(c = 0; c<list.length;c++){
       if(list[c].tagName == 'NAV'){
         list[c].classList.replace('navbar-light','navbar-dark');
       }
       list[c].classList.replace('bg-light','bg-dark');
     }
-    document.getElementById('corpo').classList = "bg-dark container text-light";
-    z++;
   }catch(error){
 return;
 }
-}while(z<=150)
 }
